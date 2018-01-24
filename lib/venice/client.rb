db45f5346546bcc6532b4ca005e91eb81563958c
Faisal Ali <faisal@rageon.com>
2018-01-24 15:44:41 -0800

    Adding back the fix

--- lib/venice/client.rb
+++ lib/venice/client.rb
@@ -48,6 +48,10 @@ module Venice
         receipt.latest_receipt_info = []
         if json['latest_receipt_info']
           latest_receipt_info_attributes = json['latest_receipt_info']
+
+          unless latest_receipt_info_attributes.is_a? Array
+            latest_receipt_info_attributes = [latest_receipt_info_attributes]
+          end
           # AppStore returns 'latest_receipt_info' even if we use over iOS 6. Besides, its format is an Array.
           latest_receipt_info_attributes.each do |latest_receipt_info_attribute|
             # latest_receipt_info format is identical with in_app
